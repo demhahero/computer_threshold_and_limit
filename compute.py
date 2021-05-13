dde623cfa3eef2591d78d4209a0fc09b3be1a24a
Ahmed <ahmedalhilo91@gmail.com>
2021-05-13 02:44:49 -0400

    Update compute.py

--- compute.py
+++ compute.py
@@ -5,14 +5,16 @@ def output_func(arr, threshold, limit):
 
     sum_val = 0
     for i, element in enumerate(arr):
+        #Subtract the threshold
         val = max(0, element - threshold)
-
+        
+        #If we reached the limited, only print zeros
         if (sum_val == limit):
             print(0)
-        elif sum_val + val < limit:
+        elif sum_val + val < limit: #If not, and the new value is still less than limit, add it to sum
             sum_val += val
             print(val)
-        else:
+        else: #Else if greater or equals to limit, only print the differece which is the last portion and make sum_val = limit
             print(limit - sum_val)
             sum_val = limit
 
@@ -24,12 +26,12 @@ if __name__ == "__main__":
     arr = []
     threshold = float(sys.argv[1])
     limit = float(sys.argv[2])
-
-    #You can change this value
-
+    
+    #Check threshold and limit within the specified ranges.
     if not ( 0 <= threshold <= 1000000000.0 and 0 <= limit <= 1000000000.0):
         exit()
 
+    #Read form the file line by line
     for line in iter(sys.stdin.readline, ''):
         line = line.replace("\n", "")
         if line.isnumeric():
@@ -42,7 +44,7 @@ if __name__ == "__main__":
         else:
             exit()
 
-
+    #Make sure that the input does not exceed 100
     if len(arr) > 100:
         exit()
 
